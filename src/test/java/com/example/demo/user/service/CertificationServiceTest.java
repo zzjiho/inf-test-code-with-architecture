b178e91cccdc6000c28a19a8cde1aae3f3286a84
zzjiho <youngyoungnim@gmail.com>
2025-03-10 20:08:40 +0900

    certificationService test 작성

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다() {
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("ziho1234567890@gmail.com", 1, "aaaaa-aaaa-aaa");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("ziho1234567890@gmail.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaaaa-aaaa-aaa");
+    }
+
+}
